c88ea4cee121a8cb4529b26ebbf0f1c452934055
klambycom <christian@klamby.com>
2013-10-31 20:11:15 +0100

    Installed github api using composer

--- src/index.php
+++ src/index.php
@@ -1,5 +1,7 @@
 <?php
 
+require_once("../vendor/autoload.php");
+
 /*
 error_reporting(E_ALL);
 ini_set('display_errors', '1');
@@ -23,7 +25,7 @@ die();
 require_once("controller/application.php");
 
 $application = new \controller\Application();
-echo $application->doApplication();
+echo $application->doRoute();
 
 /*
 $_POST['payload'] = <<<'JSON'
