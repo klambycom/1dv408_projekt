e0f7cec541492ec53dbcf314fa7f502a5350df1a
klambycom <christian@klamby.com>
2013-11-03 20:51:27 +0100

    Comments

--- src/model/AuthenticateUser.php
+++ src/model/AuthenticateUser.php
@@ -5,14 +5,32 @@ namespace model;
 require_once("../src/model/Settings.php");
 
 class AuthenticateUser extends User {
+  /**
+   * @param string $code Code from Github
+   */
   public function __construct($code) {
+    $matches = array();
+    preg_match('/access_token=(.*)&scope=(.*)&token_type=(.*)/', $result, $matches);
+
+    if (empty($matches))
+      throw new \Exception();
+
+    $_SESSION[$this->tokenName] = $matches[1];
+
+    parent::__construct();
+  }
+
+  /**
+   * @return String
+   */
+  private function postToGithub() {
     $settings = new Settings();
 
     $url = "https://github.com/login/oauth/access_token";
     $data = array(
-      'client_id' => $settings->githubId(),
+      'client_id'     => $settings->githubId(),
       'client_secret' => $settings->githubToken(),
-      'code' => $code
+      'code'          => $code
     );
     $options = array(
       'http' => array(
@@ -23,16 +41,6 @@ class AuthenticateUser extends User {
     );
 
     $context  = stream_context_create($options);
-    $result = file_get_contents($url, false, $context);
-
-    $matches = array();
-    preg_match('/access_token=(.*)&scope=(.*)&token_type=(.*)/', $result, $matches);
-
-    if (empty($matches))
-      throw new \Exception();
-
-    $_SESSION[$this->tokenName] = $matches[1];
-
-    parent::__construct();
+    return file_get_contents($url, false, $context);
   }
 }
