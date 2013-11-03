265c26579000dd3b3a7ff96818df0bb835053631
klambycom <christian@klamby.com>
2013-11-03 23:18:58 +0100

    Comments

--- src/model/Code.php
+++ src/model/Code.php
@@ -3,11 +3,24 @@
 namespace model;
 
 class Code {
+  /**
+   * @var array
+   */
   private $code;
+
+  /**
+   * @var string
+   */
   private $filename;
+
+  /**
+   * @var array
+   */
   private $raw;
 
   /**
+   * @param string $filename
+   * @param string $code
    * @throws PHPParser_Error when code cant be parsed
    */
   public function __construct($filename, $code) {
@@ -18,26 +31,47 @@ class Code {
     $this->code = $parser->parse($code);
   }
 
+  /**
+   * @return array
+   */
   public function getParsedCode() {
     return $this->code;
   }
 
+  /**
+   * @return string
+   */
   public function getFileName() {
     return $this->filename;
   }
 
+  /**
+   * @return array
+   */
   public function getRawCode() {
     return $this->raw;
   }
 
+  /**
+   * @param array $code
+   */
   public function updateCode($code) {
     $this->code = $code;
   }
 
+  /**
+   * @param int $row
+   * @return string
+   */
   public function getRow($row) {
     return $this->raw[($row == 0) ? 0 : $row - 1];
   }
 
+  /**
+   * @param string $class
+   * @param array $code Optional
+   * @return array
+   */
   public function filter($class, $code = null) {
     if ($code == null)
       $code = $this->code;
@@ -45,12 +79,5 @@ class Code {
     return array_filter($code, function ($x) use ($class) {
       return $x instanceof $class;
     });
-  }
-
-  // @todo Delete
-  public function dumpParsedCode() {
-    $nodeDumper = new \PHPParser_NodeDumper;
-    $dump = $nodeDumper->dump($this->code);
-    return '<pre>' . htmlspecialchars($dump) . '</pre>';
   }
 }
